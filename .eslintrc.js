59a1bfc9d78a22d5b250706041f6a9fd141d862b
mt <margarettom6@gmail.com>
2020-07-17 10:49:32 +0300

    Eslint set up file

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  // "extends": "eslint:recommended",
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+  },
+};
