436e2a1508fc4ef7986eda0aa73a2cdb8b3ece0f
mt <margarettom6@gmail.com>
2020-07-23 07:49:35 +0300

    Make book.save asynchronous.

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -44,8 +44,13 @@ function routes(Book) {
       book.author = req.body.title;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
-      return res.json(book);
+      
+      req.book.save((err) => {
+        if (err){
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     })
     .patch((req, res) => {
       const { book } = req;
